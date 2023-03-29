add82679f353e3751bdda0d5374ffe512247918b
ltwmori <ltwmori@gmail.com>
2023-03-29 12:49:45 +0600

    setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: [
+        new Dotenv({ systemvars: true }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/,
+            use: [
+            'style-loader',
+            'css-loader',
+            ],
+        },
+        ],
+    },
+    devServer: {
+        contentBase: __dirname + '/dist',
+        publicPath: '/',
+        watchContentBase: true,
+        open: true
+    },
+    externals: {
+        'mapbox-gl': 'mapboxgl'
+    }
+};
